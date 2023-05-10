cf195ca5aeab02d0b6155da2380925df23a4630c
amilpa <amilpa2020@gmail.com>
2023-05-10 19:10:30 +0530

    added fish config

---
+++ fish/config.fish
@@ -0,0 +1,20 @@
+set fish_greeting "Welcome amil"
+
+alias cdh "cd ~"
+alias see 'explorer.exe'
+alias vi nvim
+alias fc 'cd (find Code -type d -print | fzf)'
+alias fide 'tmux new-session -c (find Code -type d -print | fzf) -s MySession'
+alias ide3 'source ~/ide3.zsh'
+alias ide2 'source ~/ide2.zsh'
+alias tkill 'tmux kill-session'
+alias atach 'tmux attach -t'
+
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+# export NVM_DIR="$HOME/.nvm"
+# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+# [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
