313eab418001fb1bbb5b049c0b8d46ef2e941649
Amil P A <amilpa@amilpa.amilpa.com>
2023-09-24 23:37:24 +0530

    changes to linux

--- fish/config.fish
+++ fish/config.fish
@@ -1,24 +1,22 @@
-set fish_greeting ""
-
-
-alias cdh "cd ~"
-alias see 'explorer.exe'
-alias vi nvim
-alias fc 'cd (find Code -type d -print | fzf)'
-alias fide 'tmux new-session -c (find Code -type d -print | fzf) -s MySession'
-alias ide3 'source ~/ide3.zsh'
-alias ide2 'source ~/ide2.zsh'
-alias tkill 'tmux kill-session'
-alias atach 'tmux attach -t'
-
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-# comment this out after first load
+starship init fish | source
 # set --universal nvm_default_version lts
+alias tmux "tmux -u"
+alias vi "~/nvim/nvim.appimage"
+alias tkill "tmux kill-session"
 
+set -gx TERM screen-256color
+fish_vi_key_bindings
+tmux -u
 
-# export NVM_DIR="$HOME/.nvm"
-# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
-# [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
+function forward-char-and-accept-autosuggestion-if-at-end
+    set -l right_of_cursor "$(string sub "$(commandline)" --start (math (commandline --cursor) + 2))"
+    commandline -f forward-char
+    if test -z $right_of_cursor
+        commandline -f accept-autosuggestion
+    end
+end
+bind l forward-char-and-accept-autosuggestion-if-at-end
